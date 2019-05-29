05286c0d32b93a28e6ac8d5177041418a338748f
NMVikings <no.more.vikings@gmail.com>
2019-05-29 11:44:23 +0300

    Finalize rules, split rules to files

---
+++ rules/react.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: ["react"],
+
+  extends: ["plugin:react/recommended"],
+
+  rules: {
+    "react/boolean-prop-naming": "error",
+    "react/display-name": "warn", // ругается на замыкание
+    "react/no-direct-mutation-state": "error",
+    "react/no-unsafe": "error",
+    "react/prop-types": "off", // typescript вместо prop-types
+    "react/jsx-child-element-spacing": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }], // У нас .tsx
+    // "react/jsx-handler-names": "error", добавить позже
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-key": "error"
+    // "react/jsx-sort-prop": "error", добавить позже
+  },
+
+  settings: {
+    // https://github.com/yannickcr/eslint-plugin-react#configuration
+    react: {
+      version: "detect"
+    }
+  }
+};
