4d3eacb76f0be27b7fb7d18fcfd5670840acd2cd
NMVikings <no.more.vikings@gmail.com>
2020-01-23 13:49:31 +0300

    Update all dependencies

--- rules/react.js
+++ rules/react.js
@@ -13,6 +13,7 @@ module.exports = {
     'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }], // У нас .tsx
     // "react/jsx-handler-names": "error", добавить позже
     'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
     'react/jsx-key': 'error',
     'react/jsx-wrap-multilines': 'off', // prettier сам справляется
     // "react/jsx-sort-prop": "error", добавить позже
